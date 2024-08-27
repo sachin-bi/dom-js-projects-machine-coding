235bbdde01b5978e238ac260dbe32c05e78ec522
Sachinandan <sachinandanp5@gmail.com>
2024-08-27 12:35:44 +0530

    window event-keydown

--- 5-keyboard/chaiaurcode.js
+++ 5-keyboard/chaiaurcode.js
@@ -1,1 +1,41 @@
 console.log('Project 5');
+{/* <table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+</table> */}
+
+const insert = document.getElementById("insert")
+
+window.addEventListener("keydown", (e) => {
+    
+    insert.innerHTML = `
+    <table>
+  <tr>
+    <th>key</th>
+    <th>keyCode</th>
+    <th>code</th>
+  </tr>
+  <tr>
+    <td>${e.key == " " ? "space" : e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+
+</table>
+    `
+})
+
+
